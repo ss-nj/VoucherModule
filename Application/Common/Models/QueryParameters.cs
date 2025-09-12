4ae005e5a5a619fc66c4aa9f4225cc24e4fb0869
sajjad salehi <s.salehi.nj@gmail.com>
2025-09-12 15:35:18 +0330

    added person excel download

--- Application/Common/Models/QueryParameters.cs
+++ Application/Common/Models/QueryParameters.cs
@@ -9,7 +9,7 @@ namespace Application.Common.Models
     public class QueryParameters
     {
         public int Page { get; set; } = 1;
-        public int PageSize { get; set; } = 10;
+        public int PageSize { get; set; } = 50;
 
     }
 }
--- VoucherModule/Controllers/PersonsController.cs
+++ VoucherModule/Controllers/PersonsController.cs
@@ -3,6 +3,8 @@ using Application.DTOs.PersonDtos;
 using Application.Interfaces;
 using Application.Services;
 using Microsoft.AspNetCore.Mvc;
+using OfficeOpenXml;
+using OfficeOpenXml.Style;
 
 namespace Api.Controllers
 {//TODO:add versioning and rate limit
@@ -51,6 +53,44 @@ namespace Api.Controllers
             await _personService.DeleteAsync(id);
             return NoContent();
         }
+
+        [HttpGet("persons/export")]
+        public async Task<IActionResult> ExportPersonsToExcel([FromQuery] QueryParameters parameters)
+        {//by chat gbt 
+            // <-- set the license context
+            ExcelPackage.LicenseContext = LicenseContext.NonCommercial;
+
+            var persons = await _personService.GetAllAsync(parameters); 
+
+            using var package = new ExcelPackage();
+            var worksheet = package.Workbook.Worksheets.Add("Persons");
+
+            worksheet.Cells[1, 1].Value = "First Name";
+            worksheet.Cells[1, 2].Value = "Last Name";
+            worksheet.Cells[1, 3].Value = "National Id";
+            worksheet.Cells[1, 4].Value = "Phone Number";
+
+            int row = 2;
+            foreach (var p in persons.Data)
+            {
+                worksheet.Cells[row, 1].Value = p.FirstName;
+                worksheet.Cells[row, 2].Value = p.LastName;
+                worksheet.Cells[row, 3].Value = p.NationalId;
+                worksheet.Cells[row, 4].Value = p.PhoneNumber;
+                row++;
+            }
+
+            worksheet.Cells.AutoFitColumns();
+
+            var fileBytes = package.GetAsByteArray();
+            var fileName = $"Persons_{DateTime.Now:yyyyMMddHHmmss}.xlsx";
+
+            return File(fileBytes,
+                        "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
+                        fileName);
+        }
+
+        
     }
 }
 
